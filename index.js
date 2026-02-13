229bc87fa28125051c743b55dd1e203b760a4fdd
vsharma <vaishalisharmaukl@gmail.com>
2026-02-14 02:37:47 +0530

    finalizing html and js file

--- index.js
+++ index.js
@@ -1,124 +1,163 @@
-const submitBtn = document.getElementById("submit-btn")
-let descEl = document.getElementById("desc")
-let amtEl = document.getElementById("amt")
+const submitBtn = document.getElementById("submit-btn");
+const descEl = document.getElementById("desc");
+const amtEl = document.getElementById("amt");
+const expenseList = document.getElementById("expense-list");
+const formEl = document.getElementById("form");
+const totalEl = document.getElementById("total");
 
-const expenseList = document.getElementById("expense-list")
-const formEl = document.getElementById("form")
 
-
-// ---------- Error message ----------
+// Creating error message element and adding styles and classes to it
 const errorMsg = document.createElement("p");
-// errorMsg.textContent = "Enter both values";
-// errorMsg.classList.add("text-red-500", "text-sm", "mt-2", "opacity-0","" ,  "mb-2");
-errorMsg.classList.add =
-"text-red-500 text-sm  opacity-0 invisible transition-opacity duration-300 mb-2";
+// let editingId = null;
+// invisible opacity-0 transition-opacity duration-300
+errorMsg.className =
+  "text-red-500 text-sm mb-2 h-5";
 formEl.insertAdjacentElement("afterend", errorMsg);
 
-// ---------- State ----------
+// Array to hold expenses, if localStorage has data then parse it otherwise initialize with empty array
 let expensesArray = JSON.parse(localStorage.getItem("expenses")) || [];
 
-// ---------- Load from localStorage on refresh ----------
+// Passing each element of array to render function
 expensesArray.forEach(renderExpense);
+// Example of forEach: [1,2,3].forEach(n => console.log(n));
 
-// ---------- Event ----------
+updateTotal()
+
+// Event listener for submit/Add button
 submitBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    const descValue = descEl.value.trim();
-    const amtValue = Number(amtEl.value);
+  e.preventDefault();
 
-    let errorText = "";
-    if (descValue === "" || amtValue <= 0) {
-        if (descValue === "" && amtValue <= 0) {
-            errorMsg.textContent = "Please enter description and amount";
-        }
-        else if (descValue === "") {
-            errorMsg.textContent = "Please enter expense description";
-        }
-        else if (amtValue <= 0) {
-            errorMsg.textContent = "Enter amount greater than 0";
-        }
-        // reset first
-        errorMsg.classList.add("opacity-0", "invisible");
+  // prevents empty or space-only inputs like " " from being accepted and ensures clean data is stored or validated.
+  let descValue = descEl.value.trim();
+  const amtValue = Number(amtEl.value);
 
-        // force reflow (important)
-        errorMsg.offsetHeight;
+  // Input values validation
+  if (!descValue || amtValue <= 0) {
+    showError(
+      !descValue && amtValue <= 0 ? "Please enter description and amount" : !descValue ? "Please enter expense description" : "Enter amount greater than 0"
+    );
+    return;
+  }
 
-        // now show again
-        errorMsg.textContent = errorText;
-        errorMsg.classList.remove("opacity-0", "invisible");
+  // Limiting description to 25 characters and adding "..." if it exceeds the limit
+  // descValue = descValue.slice(0, 20);
+  displayText = descValue.length > 25 ? descValue.slice(0, 25) + "..." : descValue;
 
-        setTimeout(() => {
-            errorMsg.classList.add("opacity-0", "invisible");
-        }, 5000);
 
-        return;
-    }
-    // if (descValue === "" || amtValue <= 0) {
-    //     // errorMsg.classList.remove("hidden");
-    //     errorMsg.classList.remove("opacity-0", "invisible");
-    //     setTimeout(() => {
-    //         // errorMsg.classList.add("hidden");
-    //         errorMsg.classList.add("opacity-0", "invisible");
+  if (Number.isNaN(amtValue)) {
+    showError("Amount must be a number");
+    return;
+  }
 
-    //     }, 5000);
+  //creating an expense object
+  const expObj = {
+    id: Date.now(),
+    description: displayText,
+    amount: amtValue,
+  };
 
-    //     return; // stop execution
-    // }
+  //pushing to array and localStorage
+  expensesArray.push(expObj);
+  localStorage.setItem("expenses", JSON.stringify(expensesArray));
 
-    const expObj = {
-        id: Date.now(),
-        description: descValue,
-        amount: amtValue,
-    }
+  //calling render function to display the new expense in the table
+  renderExpense(expObj);
 
-    expensesArray.push(expObj)
-    localStorage.setItem("expenses", JSON.stringify(expensesArray))
+  updateTotal()
+  // reset inputs
+  descEl.value = "";
+  amtEl.value = "";
+});
 
-    renderExpense(expObj);
+// Render function to display an expense in the table
+function renderExpense(expense) {
+  const tr = document.createElement("tr");
+  tr.dataset.id = expense.id;
+  tr.className = "border-b";
+  //  <td class="flex flex-wrap gap-2 justify-center py-2">
+  //  <td class="amt py-2">${expense.amount}</td>
+  tr.innerHTML = `
+    <td class="desc py-2">${expense.description}</td>
+   <td class="amt py-2">${formatINR(expense.amount)}</td>
+   <td class="py-2 flex gap-2 justify-center">
+      <button class="edit bg-blue-500 text-white px-3 py-1 rounded text-sm hover:bg-blue-600">📝</button>
+      <button class="remove bg-red-500 text-white px-3 py-1 rounded text-sm hover:bg-red-600">✘</button>
+    </td>
+  `;
 
-    descEl.value = "";
-    amtEl.value = "";
+  expenseList.appendChild(tr);
+}
 
-})
+// showError function to display error messages
+function showError(message) {
+  errorMsg.textContent = message;
+  errorMsg.classList.remove("invisible", "opacity-0");
 
-    function renderExpense(expensesArray) {
-        
-        const trEl = document.createElement("tr")
-        trEl.classList.add("border-b")
-
-        const descTd = document.createElement("td")
-        descTd.textContent = expensesArray.description
-        descTd.className = "py-2";
-
-        const amtTd = document.createElement("td");
-        amtTd.textContent = expensesArray.amount;
-        amtTd.className = "py-2";
-
-        const actionTd = document.createElement("td");
-        actionTd.className = "py-2 flex gap-2 justify-center";
-
-        const editBtn = document.createElement("button");
-        editBtn.textContent = "📝";
-        editBtn.className = "bg-blue-500 text-white px-4 py-2 rounded text-sm hover:bg-blue-600 transition";
-
-        const removeBtn = document.createElement("button");
-        removeBtn.textContent = "✘";
-        removeBtn.className = "bg-red-500 text-white px-4 py-2 rounded text-sm hover:bg-red-600 transition";
-
-        actionTd.appendChild(editBtn);
-        actionTd.appendChild(removeBtn);
-
-        trEl.appendChild(descTd);
-        trEl.appendChild(amtTd);
-        trEl.appendChild(actionTd);
-
-        expenseList.appendChild(trEl);
-
-        // trEl.appendChild(descTd)
-        // trEl.appendChild(amtTd)
-
-        // expenseList.appendChild(trEl)
-    }
+  setTimeout(() => {
+    errorMsg.classList.add("opacity-0", "invisible");
+  }, 5000);
+}
 
 
 
+expenseList.addEventListener("click", function (e) {
+  // to find the closest parent <tr> element of the clicked target, which represents the entire row of the expense item in the table. 
+  // This allows us to identify which expense item is being interacted with, whether it's for editing or removing.
+  const row = e.target.closest("tr");
+
+  // If click wasn’t inside a row → stop execution. 
+  // This prevents errors that would occur if we tried to access properties of a non-existent row when clicking outside of the expense items.
+  if (!row) return;
+
+  const id = Number(row.dataset.id);
+
+  if (e.target.classList.contains("remove")) {
+    expensesArray = expensesArray.filter(exp => exp.id !== id);
+    localStorage.setItem("expenses", JSON.stringify(expensesArray));
+    updateTotal()
+    row.remove();
+  }
+
+  if (e.target.classList.contains("edit")) {
+    const row = e.target.closest("tr");
+    const id = Number(row.dataset.id);
+
+    // 1️⃣ Fill inputs
+    const desc = row.querySelector(".desc").textContent;
+    // const amt = row.querySelector(".amt").textContent;
+    const amt = row.querySelector(".amt").textContent
+      .replace(/[₹,]/g, "")
+      .trim();
+
+
+    descEl.value = desc;
+    amtEl.value = amt;
+
+    // 2️⃣ REMOVE old expense immediately
+    expensesArray = expensesArray.filter(exp => exp.id !== id);
+    localStorage.setItem("expenses", JSON.stringify(expensesArray));
+
+    updateTotal()
+
+    // 3️⃣ Remove row from UI
+    row.remove();
+  }
+
+});
+
+
+function updateTotal() {
+  const total = expensesArray.reduce((sum, exp) => sum + exp.amount, 0);
+  // totalEl.textContent = `₹${total}`;   --to display total without formatting
+  // totalEl.textContent = `₹${total.toLocaleString("en-IN")}`;  -- to display total with commas as thousand separators but without decimal places
+  totalEl.textContent = formatINR(total);
+
+
+}
+// To format the amount in Indian Rupees format with commas as thousand separators and two decimal places.
+function formatINR(value) {
+  return `₹${Number(value).toLocaleString("en-IN", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  })}`;
+}
